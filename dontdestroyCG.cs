7cc1287fb9aae1bb85c0938a722ac6cd42af3f27
Truongs <daniel.truong@sydstu.catholic.edu.au>
2017-06-20 23:33:31 +1000

    Update dontdestroyCG.cs

--- dontdestroyCG.cs
+++ dontdestroyCG.cs
@@ -8,9 +8,9 @@ public class dontdestroyCG : MonoBehaviour
 	// Use this for initialization
 	void Awake()
 	{ 
-		GameObject[] objs = GameObject.FindGameObjectsWithTag ("musicCG");
+		GameObject[] objs = GameObject.FindGameObjectsWithTag ("musicCG"); //finds gameobjects with tag musiccg
 		if (objs.Length > 1)
-			Destroy (this.gameObject);
+			Destroy (this.gameObject); //if there is more then two, destroy it
 
 		DontDestroyOnLoad (this.gameObject);
 
