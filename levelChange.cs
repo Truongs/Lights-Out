3849d5f96df07918ac427ae4edc26355fe2edee5
Truongs <daniel.truong@sydstu.catholic.edu.au>
2017-06-20 23:29:21 +1000

    Update levelChange.cs

--- levelChange.cs
+++ levelChange.cs
@@ -18,14 +18,14 @@ public class levelChange : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if (hasEnded && Input.GetKeyDown (KeyCode.E)) 
+		if (hasEnded && Input.GetKeyDown (KeyCode.E)) //checks if the level has ended and if the key E is pressed 
 		{
 			print ("'E' pressed, Player advance into next level");
 			Time.timeScale = 1f;
 			SceneManager.LoadScene(nextlevel);
 		}
 	}
-	void OnTriggerEnter2D(Collider2D other)
+	void OnTriggerEnter2D(Collider2D other) //checks if the player has passed the trigger
 	{
 		if (other.tag == "Player") 
 		{
