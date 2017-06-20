5f0c8f1733516eb8f1089adeae78b3e2e6dc01b9
Truongs <daniel.truong@sydstu.catholic.edu.au>
2017-06-20 23:32:48 +1000

    Update dontdestroySt1.cs

--- dontdestroySt1.cs
+++ dontdestroySt1.cs
@@ -8,9 +8,9 @@ public class dontdestroySt1 : MonoBehaviour
 	// Use this for initialization
 	void Awake()
 	{ 
-		GameObject[] objs = GameObject.FindGameObjectsWithTag ("music1");
+		GameObject[] objs = GameObject.FindGameObjectsWithTag ("music1"); //finds gameobjects with tag music2
 		if (objs.Length > 1)
-			Destroy (this.gameObject);
+			Destroy (this.gameObject); //if there is two, destroy the object
 
 		DontDestroyOnLoad (this.gameObject);
 
